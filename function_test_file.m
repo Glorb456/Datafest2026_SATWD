7cb77d485cabd4487183b96bc98b78b3a3d03af9
AkilPugal07 <akilpugalenthi@gmail.com>
2026-04-11 16:05:07 -0400

    Changed function_test_file

--- function_test_file.m
+++ function_test_file.m
@@ -1,3 +1,11 @@
 clear;
 encounters_drug_related = find_diabetes_encounters(); 
 person_ID = encounter2person(encounters_drug_related);
+
+%% 
+
+encounters_data = readtable("encounters.csv");
+
+
+patient_ID = 
+% testing filter patients function
