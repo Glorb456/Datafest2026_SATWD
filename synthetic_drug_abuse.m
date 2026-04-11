6de5548d2620e9b1561ae259071deef89626d5f6
Glorb456 <glorb458@gmail.com>
2026-04-11 14:47:44 -0400

    found indexes of encounters with diabetes patients

--- synthetic_drug_abuse.m
+++ synthetic_drug_abuse.m
@@ -8,7 +8,7 @@ diag_key_array = [];
 
 %getting keys for all synthetic narcotics diagnosis
 for i = 1:length(diag_name)
-    if contains(diag_name(i), "synthetic narcotics")
+    if contains(diag_name(i), "diabetes", "IgnoreCase", true)
         num_synth_diag = num_synth_diag + 1; 
         diag_key_array(end+1) = diag_key_data(i);
     end 
@@ -17,18 +17,13 @@ diag_key_array = diag_key_array';
 
 encounters = readtable('encounters.csv');
 
-% primary_keys = lower(strip(string(encounters.PrimaryDiagnosisKey)));
-% target_keys  = lower(strip(string(diag_key_array)));
-% 
-% is_drug_related = ismember(primary_keys, target_keys);
-% encounters_drug_related = encounters.EncounterKey(is_drug_related);
-% 
-
 primary_diag_key = encounters.PrimaryDiagnosisKey;
 encounter_key = encounters.EncounterKey; 
 encounters_drug_related = [];
 for i = 1:length(primary_diag_key)
-    if ismember(primary_diag_key(i), diag_key_array,'IgnoreCase', true) == 1
+    if ismember(primary_diag_key(i), diag_key_array) == 1
         encounters_drug_related(end+1) = encounter_key(i);
     end 
 end 
+
+encounters_drug_related = encounters_drug_related';
